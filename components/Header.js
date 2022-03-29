bbdaf868f870f30975f078f8359171ae42b88c4b
Matthew T Leiman <matthewtleiman@gmail.com>
2022-03-29 14:58:14 -0600

    Header completed

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,62 @@
 import React from 'react'
 import Image from 'next/image'
+import HeaderIcon from './HeaderIcon';
+import {
+    BellIcon,
+    ChatIcon,
+    HomeIcon,
+    ChevronDownIcon,
+    UserGroupIcon,
+    ViewGridIcon
+} from '@heroicons/react/solid';
+import {
+    FlagIcon,
+    PlayIcon,
+    SearchIcon,
+    ShoppingCartIcon
+} from '@heroicons/react/outline'; 
+
 
 function Header() {
   return (
-      <div>
-            <h1>Header</h1>
+      <div className="sticky top-0 z-50 bg-white flex items-center 
+      p-2 lg:px-5 shadow-md">
             {/* Left */}
-        <div>
+        <div className='flex items-center'>
             <Image src="https://links.papareact.com/5me"
             width={40}
             height={40}
             layout="fixed"/>
+        <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
+            <SearchIcon className='h-6 text-gray-600'/>
+            <input className="hidden flex md:inline-flex ml-2 items-center bg-transparent 
+            outline-none placeholder-gray-500 flex-shrink" 
+            type='text'
+            placeholder='Search Facebook' />
+        </div>    
         </div>
             {/* Center */}
+            <div className='flex justify-center flex-grow'>
+                <div className='flex space-x-6 md: space-x-2'>
+                    <HeaderIcon active Icon={HomeIcon} />
+                    <HeaderIcon Icon={FlagIcon} />
+                    <HeaderIcon Icon={PlayIcon} />
+                    <HeaderIcon Icon={ShoppingCartIcon} />
+                    <HeaderIcon Icon={UserGroupIcon} />
+
+                </div>
+            </div>
 
             {/* Right */}
+            <div className='flex items-center sm:space-x-2 justify-end'>
+                {/* Profile pic */}
+                <p className=" whitespace-nowrapfont-semibold pr-3">Matthew Leiman</p>
+                <ViewGridIcon className="icon"/>
+                <ChatIcon className="icon"/>
+                <BellIcon className="icon"/>
+                <ChevronDownIcon className="icon"/>
+                {/*FIXME: Change custom layer css from md to xl */}
+            </div>
       </div>   
   )
 }
