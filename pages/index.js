c75803c3044e53953e9fa5c35272cde47e384750
Matthew T Leiman <matthewtleiman@gmail.com>
2022-03-28 19:57:46 -0600

    Starting header component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -10,7 +10,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Lets build Facebook</h1>
+      
+      <Header/>
+      <main>
+        {/* SideBar */}
+        {/* Feed*/}
+        {/* Widgets */}
+      </main>
     </div>
   )
 }
